7bdc49271e7633e670827d8b19376b729fe8d215
charles <565482647@qq.com>
2021-06-15 16:05:30 +0800

    分支提交4

--- src/main/java/com/wu/GitTest.java
+++ src/main/java/com/wu/GitTest.java
@@ -9,5 +9,6 @@ public class GitTest {
         System.out.println("hello");
         System.out.println("helloHub");
         System.out.println("helloHub33");
+        System.out.println("helloGit4");
     }
 }
