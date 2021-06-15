14a0c89c0aedc4b202e8e3871286f71b05dc3640
charles <565482647@qq.com>
2021-06-15 15:52:38 +0800

    测试提交

---
+++ src/main/java/com/wu/GitTest.java
@@ -0,0 +1,11 @@
+package com.wu;
+
+/**
+ * @author wuyimin
+ * @create 2021-06-15-
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
